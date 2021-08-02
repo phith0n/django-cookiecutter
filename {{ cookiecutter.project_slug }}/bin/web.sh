6a05a1665dae7d1c680bec1fc638ff8d52e65f6b
phithon <phith0n@users.noreply.github.com>
2021-08-02 17:20:39 +0800

    fixed: wrong web.sh

--- {{ cookiecutter.project_slug }}/bin/web.sh
+++ {{ cookiecutter.project_slug }}/bin/web.sh
@@ -2,18 +2,13 @@
 
 set -eo pipefail
 
-if [ ! -d /data/static ]; then
-  mkdir -p /data/static
-fi
-
-if [ ! -d /data/media ]; then
-  mkdir -p /data/media
-fi
-
-mkdir -p data/cache data/logs data/media data/sqlite3 data/static
-chown nobody:nobody -R data/
-
 ./manage.py collectstatic --no-input
 ./manage.py migrate --no-input
 
-gunicorn {{ cookiecutter.project_slug }}.wsgi -w 2 -k gevent -u nobody -g nogroup -b 0.0.0.0:8080
+dirs=('cache' 'logs' 'media' 'sqlite3' 'statiic')
+for dir in "${dirs[@]}"; do
+  mkdir -p "data/${dir}"
+  chown -R nobody:nogroup "data/${dir}"
+done
+
+gunicorn -w 2 -k gevent -u nobody -g nogroup -b 0.0.0.0:8080 {{ cookiecutter.project_slug }}.wsgi
